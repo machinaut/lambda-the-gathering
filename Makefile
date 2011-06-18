da686dc14fd9b240f238ca224f9ee71e09e2a895
Alex Ray <ajray@us.ibm.com>
2011-06-18 16:16:17 -0500

    brb pwntesting hotel wireless

--- Makefile
+++ Makefile
@@ -3,4 +3,8 @@ include $(GOROOT)/src/Make.inc
 TARG=ajr
 GOFILES=ajr.go card.go
 
+gofmt:
+	gofmt -spaces -tabindent=false -tabwidth=4 -w card.go 
+	gofmt -spaces -tabindent=false -tabwidth=4 -w ajr.go
+
 include $(GOROOT)/src/Make.cmd
--- card.go
+++ card.go
@@ -1,8 +1,22 @@
 package main
 
 type Card struct {
-    Id      int //
-    Short byte // Shortname
+    Id    int    //
+    Short byte   // Shortname
     Name  string // long name
+    Desc  string // long name
 }
-//var Deck []Card {
+
+var Deck = []Card{
+    Card{0, 'z', "zero", "λx.0"},
+    Card{1, 'I', "I", "λx.x"},
+    Card{2, 's', "succ", "λx.x+1"},
+    Card{3, 'd', "dbl", "λx.x+x"},
+    Card{4, 'g', "get", "λi.ρ[i]"},
+    Card{5, 'p', "put", "λx.I"},
+    Card{6, 'S', "S", "λf.λg.λx.fx(gx)"},
+    Card{7, 'K', "K", "λx.λy.x"},
+    Card{8, 'i', "inc", "λi.I v[i]←v[i]+1"},
+    Card{9, 'd', "dec", "λi.I ν[255-i]←ν[255-i]-1"},
+    Card{10,'a', "attack", "λi.λj.λn.fx(gx)"},
+}
